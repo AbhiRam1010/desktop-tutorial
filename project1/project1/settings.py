25b18e0e20f07bf4886b78e90ade37b46619426e
AbhiRam1010 <141568371+AbhiRam1010@users.noreply.github.com>
2024-06-17 22:05:56 +0530

    Update settings.py

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "WYpwkxOYooObsvrWGERRAuOTFmiKpNLb",
+        "HOST": "viaduct.proxy.rlwy.net",
+        "PORT": "55065",
     }
 }
+
 
 
 # Password validation
